01b9a41bf0ee289fd5a01ab040d9f6cda054afc4
wwishh <wson1948@gmail.com>
2023-12-29 11:57:31 +0900

    day1229

--- src/baekjoon/Bj_2563.java
+++ src/baekjoon/Bj_2563.java
@@ -9,8 +9,32 @@ public class Bj_2563 {
 		Scanner sc = new Scanner(System.in);
 
 		int n = sc.nextInt();
-		sc.nextLine();
 		
+		int [][] arr = new int[100][100];
+		
+		int area = 0;
+		
+		for(int i=0; i<n; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			
+			for(int j=x; j<x+10; j++) {
+				for(int k=y;k<y+10;k++) {
+					arr[j][k] = 1;
+				}
+			}
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr[0].length; j++) {
+				if(arr[i][j]==1) {
+					area++;
+				}
+					
+			}
+		}
+		
+		System.out.println(area);
 
 	}
 
