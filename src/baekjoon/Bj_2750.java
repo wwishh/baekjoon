ab4c0d7084f55140c6c315510cdc96621e97206b
wwishh <wson1948@gmail.com>
2024-01-03 21:14:36 +0900

    day0103_2

---
+++ src/baekjoon/Bj_2750.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Bj_2750 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr.length; j++) {
+				if(arr[i]<arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
