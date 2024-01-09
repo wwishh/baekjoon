9e304e2532573b2d80ed3fed9aac9a4fb9c58c11
wwishh <wson1948@gmail.com>
2024-01-09 12:31:27 +0900

    day0109

---
+++ src/baekjoon/Bj_24266.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Bj_24266 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		long n = sc.nextLong();
+
+		System.out.println(n * n * n);
+		System.out.println(3);
+	}
+
+}
